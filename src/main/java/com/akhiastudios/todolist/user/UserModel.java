f0e823e577487ad1a8fd2493bd492d615d4cc296
Fernando <fernandooogoncalves@gmail.com>
2025-10-20 16:08:38 -0300

    JPA adicionado

--- src/main/java/com/akhiastudios/todolist/user/UserModel.java
+++ src/main/java/com/akhiastudios/todolist/user/UserModel.java
@@ -1,12 +1,28 @@
 package com.akhiastudios.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users") // fala para o h2 que essa classe é uma tabela 
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
