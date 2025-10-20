6722ddecb3314a44f60eff9751592233a437c4d9
Fernando <fernandooogoncalves@gmail.com>
2025-10-20 17:46:17 -0300

    criptografia de senhas com bcrypt adicionado

--- src/main/java/com/akhiastudios/todolist/user/UserController.java
+++ src/main/java/com/akhiastudios/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -25,6 +27,11 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe"); // retorna STATUS CODE 400
         }
         
+        var passwordHash = BCrypt.withDefaults() // criptografa a senha com o BCrypt, utilizando 12 de força de senha e o local da senha
+                .hashToString(12, userModel.getPassword().toCharArray()); // transforma tudo em um array de char pq o bcrypt precisa ver chars para trablhar
+
+        userModel.setPassword(passwordHash); // criptograda a senha
+
         var userCreated = this.userRepository.save(userModel); // o var atomaticamente define o tipo de variavel analisando que atribuicao foi dada a variavel, diminuindo a verbosidade do codigo
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
